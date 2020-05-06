339c56a8cf32227a2464465cfdf7da3c59c81427
Jakub Stroiński <bukajxd11@gmail.com>
2020-05-06 20:40:20 +0200

    Wyczyszczenie maina

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 
 int main() {
-	int mrówka = 100;
-	cout << "Mrowisko sim 3000" << endl;
-	for (int i = 0; i < 100; i++)
-	{
-		spawn_mrówencja();
-	}
+
 }
 
 struct mruwa {
 	char type;		//Typ mruwy
 	int hp;			//Aktualne HP danej mruwy
-	int maxhp;		//Maksymalne HP tej mruwy, za³ó¿my ¿e mrówka ka¿dego typu ma inne HP. Wojownik koks, robotnica a hita itd
-	int strenght;	//Si³a gryz¹cej mruwencji
-	int speed;		//Prêdkoœæ poruszania siê
-
+	int maxhp;		//Maksymalne HP tej mruwy, zalozmy ze mrowka kazdego typu ma inne HP. Wojownik koks, robotnica na hita itd
+	int strenght;	//Sila gryzacej mruwencji
+	int speed;		//Predkosc poruszania sie
 };
