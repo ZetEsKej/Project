beb653e19216dbeab75b5cf8a3d2d6083cb665ba
FirentisTFW <jkb.pawel@gmail.com>
2020-05-09 19:31:44 +0200

    Paweł Jakubowski 09.05.2020, zmiany w changelogu

---
+++ Timber/ant.cpp
@@ -0,0 +1,12 @@
+//#include "ant.h"
+//
+//int movementSpeed = 0.2;
+//
+//Ant::ant() {
+//
+//}
+//
+//
+//Ant::~ant() {
+//
+//}
---
+++ Timber/ant.h
@@ -0,0 +1,11 @@
+//#pragma once
+//class Ant {
+//
+//public:
+//
+//	int movementSpeed;
+//
+//	Ant();
+//	~Ant();
+//};
+//
