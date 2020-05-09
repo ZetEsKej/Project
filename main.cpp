a58e59724953d55b3d979651f6257316798759c0
Kamil Szymkowiak <szymkowiak.k2001@gmail.com>
2020-05-09 18:23:37 +0200

    Dodano obsługę kamery do main i inne rzeczy

--- main.cpp
+++ main.cpp
@@ -8,7 +8,49 @@ int main() {
 	
 	// Renderowanie okna gry
 	VideoMode vm(1920, 1080);
-	CreateWindow window(vm, "Age of Ants", Style::Fullscreen);
+	RenderWindow window(vm, "Age of Ants", Style::Fullscreen);
+	View view = window.getView();
+	Texture textureBackground;
+	//Za³adowanie pliku do tekstury
+	textureBackground.loadFromFile("graphics/background.jpg");
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
+	//Tworzenie sprite chmurki
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/Cloud.png");
+	Sprite spriteCloud;
+	spriteCloud.setTexture(textureCloud);
+	spriteCloud.setPosition(0, 0);
+	while (window.isOpen())
+	{
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		if (Keyboard::isKeyPressed(Keyboard::M))
+		{
+			view.zoom(0.5f);
+
+		}
+		if (Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			view.move(4.f, 0.f);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			view.move(-4.f, 0.f);
+		}
+
+		window.clear();
+		//Rysowanie sceny gry tutaj - Draw Game Scene Here
+		window.draw(spriteBackground);
+		//Rysowanie chmury
+		window.draw(spriteCloud);
+		window.setView(view);
+		//Pokaz wszystko co narysowaliœmy - Show everything we drew
+		window.display();
+	}
 }
 
 struct mruwa {
