42172b5591b5718a836f4c9cbb49082ce04857d4
itamm15 <38294529+itamm15@users.noreply.github.com>
2020-05-15 23:01:52 +0200

    Add files via upload

---
+++ SFML/15.05 - Sarnecki Osiński/Ant_Org/Ant_Org.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "Game.h"
+#define SCREEN_HIGH 1024
+#define SCREEN_WIDTH 1000
+
+int main(){
+    Game game;
+
+    game.run();
+    return 0;
+}
