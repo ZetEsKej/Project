b8954fe660f10fa212f93ce2f73bb0fdb84e3c6e
FirentisTFW <jkb.pawel@gmail.com>
2020-05-24 19:39:56 +0200

    Update changelogu

--- AgeOfAnts/lifebar.cpp
+++ AgeOfAnts/lifebar.cpp
@@ -25,7 +25,7 @@ public:
 		rect.setSize(sf::Vector2f(float((ant->rect.getSize().x - 80) * (ant->hp/maxHp)), 5.0f));
 		rect.setPosition(ant->rect.getPosition().x + 40, 1020);		// umieszczenie paska pod mrówk¹
 
-		if (rect.getSize().x < 3) {								// gdy hp jest bardzo niskie, pasek staje siê niewidoczny. Ta funkcja sprawia, ¿e ma on chocia¿ minimaln¹ gruboœæ
+		if (rect.getSize().x < 3) {								// gdy hp jest bardzo niskie, pasek staje siê niewidoczny. Ten if sprawia, ¿e ma on chocia¿ minimaln¹ gruboœæ
 			rect.setSize(sf::Vector2f(3.0f, 5.0f));
 		}
 	}
@@ -34,7 +34,7 @@ public:
 		rect.setSize(sf::Vector2f(0.005f * castle->hp, 5.0f));
 		rect.setPosition(castle->rect.getPosition().x + 50, castle->rect.getPosition().y + castle->rect.getSize().y + 20);		// umieszczenie paska pod zamkiem
 
-		if (rect.getSize().x < 3) {					// gdy hp jest bardzo niskie, pasek staje siê niewidoczny. Ta funkcja sprawia, ¿e ma on chocia¿ minimaln¹ gruboœæ
+		if (rect.getSize().x < 3) {					// gdy hp jest bardzo niskie, pasek staje siê niewidoczny. Ten if sprawia, ¿e ma on chocia¿ minimaln¹ gruboœæ
 			rect.setSize(sf::Vector2f(3.0f, 5.0f));
 		}
 	}
